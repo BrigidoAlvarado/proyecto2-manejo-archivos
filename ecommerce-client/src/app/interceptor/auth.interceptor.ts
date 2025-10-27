baed53d69df939e02ad7c6da1b6e17d62e27b471
Brigido Alvarado <brigidoavarado@gmail.com>
2025-10-27 03:34:52 -0600

    without header authorization

--- ecommerce-client/src/app/interceptor/auth.interceptor.ts
+++ ecommerce-client/src/app/interceptor/auth.interceptor.ts
@@ -12,16 +12,15 @@ export const authInterceptor: HttpInterceptorFn = (req, next) => {
     return next(req)
   }
 
-
+/*
   if (token) {
     const cloned = req.clone({
       setHeaders: {
         Authorization: `Bearer ${token}`,
-
       },
     })
     return next(cloned)
   }
-
+*/
   return next(req)
 }
